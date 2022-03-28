390dfaad124d4d247f82c404efdd8c36f7fbdfe0
TeslaIh <87144117+TeslaIh@users.noreply.github.com>
2022-03-28 16:00:43 +0300

    first try
    something wrong with "textContent"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-import React from "react";
-import FeedbackBox from "./components/FeedbackBox/FeedbackBox";
+import React, { useState } from 'react';
+import Statistic from './components/Statistic';
+import FeedbackVar from './components/FeedbackVar';
+import Section from './components/Section';
+import Notification from './components/Notification';
 
-class App extends React.Component {
-  render() {
-    return <FeedbackBox />;
-  }
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onMakeFeedback = e => {
+    const changeValue = e.target.textContent;
+
+    switch (changeValue) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const total = good + neutral + bad;
+
+  const countPositive = () => {
+    return Math.round((good / total) * 100);
+  };
+
+  const options = { good, neutral, bad };
+
+  return (
+    <>
+      <Section title="Leave feedback, please">
+        <FeedbackVar options={options} onMakeFeedback={onMakeFeedback} />
+      </Section>
+
+      <Section title="Statistic">
+        {total > 0 ? (
+          <Statistic
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positive={countPositive()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </>
+  );
 }
-
-export default App;
